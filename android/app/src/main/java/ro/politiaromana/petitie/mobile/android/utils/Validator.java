a5c0cac172168ea03fe734bbcfd1251ff67c2f3e
Andrei <office@andreiverdes.com>
2016-12-05 23:49:32 +0200

    android - Closes #4 - Added CNP validation

--- android/app/src/main/java/ro/politiaromana/petitie/mobile/android/utils/Validator.java
+++ android/app/src/main/java/ro/politiaromana/petitie/mobile/android/utils/Validator.java
@@ -28,9 +28,22 @@ public final class Validator {
         return EMAIL_ADDRESS_PATTERN.matcher(emailAddress).matches();
     }
 
-    public static boolean isValidCNP(@NonNull final String cnp) {
-        // TODO: implement this
-        return true;
+    public static boolean isValidCNP(@NonNull final String cnpString) {
+        int[] cnp = new int[13];
+        int[] control = {2,7,9,1,4,6,3,5,8,2,7,9};
+        if (cnpString.length() != 13) {
+            return false;
+        }
+        char[] cnpArray = cnpString.toCharArray();
+        for (int i = 0; i < 13; i++) {
+            cnp[i] = Character.getNumericValue(cnpArray[i]);
+        }
+        int sum = 0;
+        for (int i = 0; i < 12; i++) {
+            sum += (cnp[i] * control[i]);
+        }
+        int remainder = sum % 11 == 10 ? 1 : sum % 11;
+        return remainder == cnp[12];
     }
 
     public static boolean isValidROPhoneNumber(@NonNull final String phoneNumber) {
