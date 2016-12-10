733a3ca4bbfca5485b2bf8520e8c718b4050ba84
Ilea Cristian <ileacristian@gmail.com>
2016-12-11 01:03:53 +0200

    initial iOS project commit

---
+++ ios/PolitiaRomanaPetitii/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PolitiaRomanaPetitii
+//
+//  Created by Cristian Ilea on 12/10/16.
+//  Copyright © 2016 GovITHub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
