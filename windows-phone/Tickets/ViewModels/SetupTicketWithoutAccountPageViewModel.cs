586fb316984a70f4ac1e89a02f79238b5fcf69b9
Timotei Dolean <timotei21@gmail.com>
2017-01-16 15:39:45 +0200

    Add the SendTicketWithoutAccount page

--- windows-phone/Tickets/ViewModels/SetupTicketWithoutAccountPageViewModel.cs
+++ windows-phone/Tickets/ViewModels/SetupTicketWithoutAccountPageViewModel.cs
@@ -4,5 +4,16 @@ namespace Tickets.ViewModels
 {
     public class SetupTicketWithoutAccountPageViewModel : PropertyChangedBase
     {
+        private readonly INavigationService _navigationService;
+
+        public SetupTicketWithoutAccountPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public void NextStep()
+        {
+            _navigationService.For<SendTicketWithoutAccountPageViewModel>().Navigate();
+        }
     }
 }
