62908fee3352a8751de57ef73c6f2c6c2de07b05
Timotei Dolean <timotei21@gmail.com>
2017-01-30 21:21:16 +0200

    Add counties

--- windows-phone/Tickets/Models/Ticket.cs
+++ windows-phone/Tickets/Models/Ticket.cs
@@ -1,5 +1,6 @@
 ﻿using Caliburn.Micro;
 using System;
+using System.Collections.Generic;
 
 namespace Tickets.Models
 {
@@ -47,5 +48,51 @@ namespace Tickets.Models
             get { return _answerType; }
             set { _answerType = value; NotifyOfPropertyChange(); }
         }
+
+        public static readonly List<string> Counties = new List<string>
+        {
+            "Alba",
+            "Arad",
+            "Argeș",
+            "Bacău",
+            "Bihor",
+            "Bistrița-Năsăud",
+            "Botoșani",
+            "Brăila",
+            "Brașov",
+            "București",
+            "Buzău",
+            "Călărași",
+            "Caraș-Severin",
+            "Cluj",
+            "Constanța",
+            "Covasna",
+            "Dîmbovița",
+            "Dolj",
+            "Galați",
+            "Giurgiu",
+            "Gorj",
+            "Harghita",
+            "Hunedoara",
+            "Ialomița",
+            "Iași",
+            "Ilfov",
+            "Maramureș",
+            "Mehedinți",
+            "Mureș",
+            "Neamț",
+            "Olt",
+            "Prahova",
+            "Sălaj",
+            "Satu Mare",
+            "Sibiu",
+            "Suceava",
+            "Teleorman",
+            "Timiș",
+            "Tulcea",
+            "Vaslui",
+            "Vîlcea",
+            "Vrancea"
+        };
     }
 }
--- windows-phone/Tickets/ViewModels/SetupTicketWithoutAccountPageViewModel.cs
+++ windows-phone/Tickets/ViewModels/SetupTicketWithoutAccountPageViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using Caliburn.Micro;
+using System.Collections.Generic;
+using Tickets.Models;
 
 namespace Tickets.ViewModels
 {
     public class SetupTicketWithoutAccountPageViewModel : PropertyChangedBase
     {
         private readonly INavigationService _navigationService;
+        private string _county = "București";
 
         public SetupTicketWithoutAccountPageViewModel(INavigationService navigationService)
         {
@@ -15,5 +18,21 @@ namespace Tickets.ViewModels
         {
             _navigationService.For<SendTicketWithoutAccountPageViewModel>().Navigate();
         }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PersonalNumericalCode { get; set; }
+        public string Address { get; set; }
+
+        public string Telephone { get; set; }
+
+        public string County
+        {
+            get { return _county; }
+            set { _county = value; NotifyOfPropertyChange(); }
+        }
+
+        public List<string> Counties => Ticket.Counties;
     }
 }
